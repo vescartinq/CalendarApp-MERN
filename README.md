872b656c7f7869ed72950773d5bd908edeba5621
Starkin28 <victoreq00@gmail.com>
2021-04-14 13:23:46 +0200

    testing front in progress

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # CalendarApp-MERN
+
+---
+
+## Table of Contents
+
+1. [General Info](#general-info)
+2. [Web Example](#web-example)
+3. [Technologies](#technologies)
+4. [Installation](#installation)
+
+---
+
+## 1. General Info
+
+This app has been created with React using REDUX architecture, and is built with
+the following technologies:
+
+- REACT
+- REACT-REDUX
+- NODE
+- EXPRESS
+- MONGO DB
+- TDD - JEST-ENZYME
+
+## 2. Web Example
+
+You can enter to the app here :
+
+- https://mern-calendar-app-venn.herokuapp.com/
+
+```
+ user: ejemplo@ejemplo.com
+ password: 123456
+```
+
+## 3. Technologies
+
+A list of technologies used within the project:
+
+- [REACT]: Version 17.0.1
+- [REACT-REDUX]: Version 7.2.0
+- [EXPRESS]: Version 4.17.1
+- [MONGOOSE]: Version 5.9.18
+- [ENZYME]: Version 3.11.0
+
+## 4. Installation
+
+A little intro about the installation:
+
+```
+$ git clone https://github.com/vescartinq/NOALONE-Skylab-final-project-REACT-REDUX.git
+
+$ cd backend
+$ npm install
+$ npm start
+
+$ cd frontend
+$ npm install
+$ npm start
+```
---
+++ frontend/src/router/AppRouter.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+
+export const AppRouter = () => {
+  const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(startChecking());
+  }, [dispatch]);
+
+  if (checking) {
+    return <h5>Please Wait...</h5>;
+  }
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <PublicRoute
+            exact
+            path="/login"
+            component={LoginScreen}
+            isAuthenticated={!!uid} //doble negación lo convierte en booleano
+          />
+
+          <PrivateRoute
+            exact
+            path="/"
+            component={CalendarScreen}
+            isAuthenticated={!!uid}
+          />
+
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
